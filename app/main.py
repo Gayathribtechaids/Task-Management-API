1f243a1da63a1359f468fbfb6095b4e41b7d1dd4
Gayathribtechaids <gayathrijaishu0202@gmail.com>
2026-09-07 12:24:37 +0530

    Add user creation API

--- app/main.py
+++ app/main.py
@@ -1,8 +1,31 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, Depends
+from sqlalchemy.orm import Session
+
+from app.database import Base, engine, SessionLocal
+from app.models.user import User
+from app.schemas.user import UserCreate
+from app.services.user_service import create_user
+
+
+Base.metadata.create_all(bind=engine)
 
 app = FastAPI(title="Task Management API")
+
+
+def get_db():
+    db = SessionLocal()
+
+    try:
+        yield db
+    finally:
+        db.close()
 
 
 @app.get("/")
 def home():
     return {"message": "Task Management API is running"}
+
+
+@app.post("/users")
+def create_new_user(user: UserCreate, db: Session = Depends(get_db)):
+    return create_user(db, user)
---
+++ app/services/user_service.py
@@ -0,0 +1,17 @@
+from sqlalchemy.orm import Session
+
+from app.models.user import User
+from app.schemas.user import UserCreate
+
+
+def create_user(db: Session, user: UserCreate):
+    new_user = User(
+        name=user.name,
+        email=user.email
+    )
+
+    db.add(new_user)
+    db.commit()
+    db.refresh(new_user)
+
+    return new_user
